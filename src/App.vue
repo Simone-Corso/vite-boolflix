7efb43cc9197d5b03993da7244fab0b3c7121c1d
Simone-Corso <scorso26@gmail.com>
2023-12-20 17:31:42 +0100

    rinserito tutto in modo corretto con i file

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AppAPI from './components/AppAPI.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppMainVue from './components/AppMain.vue';
+
+
+components: {
+  AppAPI,
+  AppHeader,
+  AppMainVue,
+}
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
