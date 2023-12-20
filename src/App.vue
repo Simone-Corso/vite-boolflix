22c175d3a9cb0222233d6286f35984370f247079
Simone-Corso <scorso26@gmail.com>
2023-12-20 17:37:25 +0100

    messo in ordine i file

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
-<script setup>
+<script>
 import AppAPI from './components/AppAPI.vue';
 import AppHeader from './components/AppHeader.vue';
 import AppMainVue from './components/AppMain.vue';
 
 
-components: {
-  AppAPI,
-  AppHeader,
-  AppMainVue,
-}
+export default {
+  components: {
+    AppAPI,
+    AppHeader,
+    AppMainVue,
+  },
+};
 </script>
 
 <template>
   
+    <AppHeader />
+    <AppAPI />
+    <AppMainVue />
+
 </template>
 
 <style scoped>
