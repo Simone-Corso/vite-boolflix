4698d6ce492e7e6c3ca84fd58ea71db510d6467e
Simone-Corso <scorso26@gmail.com>
2023-12-21 11:18:53 +0100

    ho messo il click e poi il button

--- src/components/AppAPI.vue
+++ src/components/AppAPI.vue
@@ -1,11 +1,21 @@
 <template lang="">
     <div>
-       <input type="text" v-model="">
+       <input type="text" v-model="searchText">
+       <button @click="$emit('search', searchText)"> search
+
+       </button>
     </div>
 </template>
 <script>
 export default {
-    name: 'AppAPI'
+    name: 'AppAPI',
+
+    data() {
+        return {
+            searchText: '',
+            
+        }
+    },
 }
 </script>
 <style lang="">
