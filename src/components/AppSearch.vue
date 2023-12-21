8dbd1d9b5b0a792115e1a18b8a4ff47363b33b6c
Simone-Corso <scorso26@gmail.com>
2023-12-21 23:37:23 +0100

    ora manca solo aggiungere la parte dell'immagine.

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
+  <div class="search">
     <input type="text" v-model="searchText" @keyup.enter="search" @input="search" placeholder="Search">
     <button @click="search">Search</button>
     <div v-if="ilRisultato">
-      Risultati: {{ searchText }}
+    <div v-for="movie in moviesList" :key="movie.id">
+         {{ movie.original_title }} {{ movie.languageFlag }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+
+const languageFlagMap = {
+  en: '🇺🇸',
+  es: '🇪🇸',
+  fr: '🇫🇷',
+};
 
 export default {
   data() {
@@ -23,25 +31,52 @@ export default {
     search() {
       this.$emit('search', this.searchText);
 
+     
       axios.get('https://api.themoviedb.org/3/search/movie?api_key=86c594eddf3a5367752cf671fefca365&query=' + this.searchText)
         .then((response) => {
           console.log(response);
-          this.moviesList = response.data.results;
+          this.moviesList = response.data.results.map((movie) => ({
+            ...movie,
+            languageFlag: this.getLanguageFlag(movie.original_language),
+          }));
+          this.ilRisultato = true;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      
+      axios.get('https://api.themoviedb.org/3/search/tv?api_key=86c594eddf3a5367752cf671fefca365&query=' + this.searchText)
+        .then((response) => {
+          console.log(response);
+          
+          this.moviesList = this.moviesList.concat(response.data.results.map((serie) => ({
+            ...serie,
+            languageFlag: this.getLanguageFlag(serie.original_language),
+          })));
           this.ilRisultato = true;
         })
         .catch((error) => {
           console.log(error);
         });
     },
-  },
-  created() {
-   
+
+    getLanguageFlag(language) {
+      return languageFlagMap[language] || '';
+    },
   },
 };
 </script>
 
 <style scoped>
-  
+
+.search{
+display: flex;
+justify-content: center;
+align-items: center;
+flex-wrap: wrap;
+}
+
 </style>
 
 
