f925414023f24c97e5a4675dea7c7e7f0a825bde
Simone-Corso <scorso26@gmail.com>
2023-12-21 23:22:07 +0100

    ora funziona anche l'array ma risulta il problema di farli stampare tutti nella pagina.

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,20 @@
 <template lang="">
     <div>
-        
+        <article v-for="movie in movies">
+            {{movie.title}}
+        </article>
     </div>
 </template>
 <script>
 export default {
-    name: 'AppMovie'
+    name: 'AppMovie',
+
+    props: {
+        movies: {
+            type: Array,
+            required: true
+        }
+    }
 }
 </script>
 <style lang="">
--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -1,36 +1,48 @@
-
 <template>
   <div>
     <input type="text" v-model="searchText" @keyup.enter="search" @input="search" placeholder="Search">
     <button @click="search">Search</button>
     <div v-if="ilRisultato">
-  Risultati: {{ searchText }}
-</div>
+      Risultati: {{ searchText }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       searchText: '',
       ilRisultato: false,
+      moviesList: [],
     };
   },
   methods: {
     search() {
-      
       this.$emit('search', this.searchText);
+
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=86c594eddf3a5367752cf671fefca365&query=' + this.searchText)
+        .then((response) => {
+          console.log(response);
+          this.moviesList = response.data.results;
+          this.ilRisultato = true;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
   created() {
-    
+   
   },
 };
 </script>
 
 <style scoped>
-
+  
 </style>
+
 
   
