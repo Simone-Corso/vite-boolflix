0ad595e29344c9f8f8bc5291a95fa9b1a810bb76
Simone-Corso <scorso26@gmail.com>
2023-12-21 11:07:57 +0100

    nel esxport default ho messo il nome

--- src/components/AppAPI.vue
+++ src/components/AppAPI.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-    
+    name: 'AppAPI'
 }
 </script>
 <style lang="">
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-    
+    name: 'AppMovie'
 }
 </script>
 <style lang="">
